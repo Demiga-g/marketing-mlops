777394f32f53d1d3ce98a3f1f711725de76e7f7a
Demiga-g <midegageorge2@gmail.com>
2023-08-15 09:17:03 +0300

    Changed the access to data directory.

--- 03-orcherstration/01-orcherstrate.py
+++ 03-orcherstration/01-orcherstrate.py
@@ -166,8 +166,8 @@ def train_best_model(
         
         return None
     
-def main_flow(train_path: str = "./data/training_data.csv", 
-              val_path: str = "./data/validation_data.csv") -> None:
+def main_flow(train_path: str = "../00-data/training_data.csv", 
+              val_path: str = "../00-data/validation_data.csv") -> None:
     
     # Mlflow settings
     mlflow.set_tracking_uri("sqlite:///mlflow.db")
